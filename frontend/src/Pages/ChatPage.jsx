119c0f1b989aed9a68d80bc0c7f3fcbd613911bf
rehan6025 <rehanthakur6025@gmail.com>
2025-04-29 17:37:47 +0530

    created mongodb models

--- frontend/src/Pages/ChatPage.jsx
+++ frontend/src/Pages/ChatPage.jsx
@@ -1,31 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const ChatPage = () => {
+  const [chats, setChats] = useState([]);
 
-    const [chats, setChats] = useState([]);
+  const fetchChats = async () => {
+    const { data } = await axios.get("/api/chats");
+    setChats(data);
+  };
 
-    
-
-    useEffect(()=>{
-        const fetchChats = async () => {
-            const {data} = await axios.get('/api/chats')
-            setChats(data); 
-            console.log(data);
-        }
-
-        fetchChats();
-    }, []) 
-
+  useEffect(() => {
+    fetchChats();
+  }, []);
 
   return (
     <div>
-      {chats.map( ( chat )=>{
-        console.log(chat.users);
-        
-      })}
+      {chats.map((chat) => (
+        <div key={chat._id}>{chat.chatName}</div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ChatPage
+export default ChatPage;
