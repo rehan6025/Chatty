260072b4b53bc46608bb0976e97a13846018860e
rehan6025 <rehanthakur6025@gmail.com>
2025-05-11 11:04:41 +0530

    context setup

--- frontend/src/Pages/ChatPage.jsx
+++ frontend/src/Pages/ChatPage.jsx
@@ -2,25 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const ChatPage = () => {
-  const [chats, setChats] = useState([]);
-
-  const fetchChats = async () => {
-    const { data } = await axios.get("/api/chats");
-    setChats(data);
-  };
-
-  useEffect(() => {
-    fetchChats();
-  }, []);
-
-  return (
-    <div>
-      {chats.map((chat) => (
-        <div key={chat._id}>{chat.chatName}</div>
-      ))}
-      <div></div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default ChatPage;
--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -3,8 +3,19 @@ import Login from "../components/authentication/Login";
 import { Box, Container, Text, Tabs } from "@chakra-ui/react";
 import Signup from "../components/authentication/Signup";
 import { Toaster } from "@/components/ui/toaster";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    setUser(userInfo);
+    if (userInfo) {
+      navigate("/chats");
+    }
+  }, [navigate]);
+
   return (
     <Container maxW="xl" centerContent>
       <Box
